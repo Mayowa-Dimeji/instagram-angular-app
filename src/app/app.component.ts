cab9c6ed96cda4121d98554fa2309a830930a927
Mayowa Oladimeji <dimejikassie@gmail.com>
2024-01-30 09:15:35 +0000

    instagram home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './navbar/navbar.component'
 import {FooterComponent} from './footer/footer.component'
 import {FeedComponent} from './feed/feed.component'
+import {FriendsTabComponent} from './friends-tab/friends-tab.component'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,FooterComponent,FeedComponent],
+  imports: [RouterOutlet, NavbarComponent,FooterComponent,FeedComponent,FriendsTabComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/friends-tab/friends-tab.component.ts
+++ src/app/friends-tab/friends-tab.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common'
 
 @Component({
   selector: 'app-friends-tab',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './friends-tab.component.html',
   styleUrl: './friends-tab.component.css'
 })
 export class FriendsTabComponent {
-
+  friends = [
+    { id: 1, picture: "assets/bt.jpg" },
+    { id: 2, picture: "assets/c.jpg" },
+    { id: 3, picture: "assets/x.jpg" },
+    { id: 4, picture: "assets/gj.jpg" },
+    { id: 5, picture: "assets/h.jpg" },
+    { id: 6, picture: "assets/k.jpg" },
+    { id: 7, picture: "assets/ll.jpg" },
+    { id: 8, picture: "assets/o.jpg" },
+    { id: 9, picture: "assets/s.jpg" },
+    { id: 10, picture: "assets/smile.jpg" }
+  ];
+  
 }
